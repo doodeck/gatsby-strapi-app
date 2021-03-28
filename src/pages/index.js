e7f0aa070b8fbb7525244569a89377ae0755ea23
doodeck <8515450+doodeck@users.noreply.github.com>
2021-03-28 16:14:24 +0200

    Bound to strapi data source

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,54 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+// import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+// import SEO from "../components/seo"
+
+import { StaticQuery, graphql } from 'gatsby';
+
+const query = graphql`
+  query {
+    allStrapiRestaurant {
+      edges {
+        node {
+          strapiId
+          name
+          description
+        }
+      }
+    }
+    strapiCategory(strapiId: { eq: 1 }) {
+      id
+      name
+      restaurants {
+        id
+        name
+        description
+      }
+    }
+  }
+`;
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
+    <StaticQuery
+      query={query}
+      render={data => (
+        <div>
+          <ul>
+            {data.allStrapiRestaurant.edges.map(restaurant => (
+              <li key={restaurant.node.strapiId}>{restaurant.node.name}</li>
+            ))}
+          </ul>
+        <h1>{data.strapiCategory.name}</h1>
+        <ul>
+          {data.strapiCategory.restaurants.map(restaurant => (
+            <li key={restaurant.id}>{restaurant.name}</li>
+          ))}
+        </ul>
+      </div>
+    )}
     />
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
