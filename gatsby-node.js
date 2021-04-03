afeae98d14aabc92335fec4c7a9a3f175ea9a5d9
doodeck <8515450+doodeck@users.noreply.github.com>
2021-04-03 23:14:39 +0200

    categories generating pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,40 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions;
+    const result = await graphql(
+      `
+        {
+          categories: allStrapiCategory {
+            edges {
+              node {
+                name
+              }
+            }
+          }
+        }
+      `
+    );
+  
+    if (result.errors) {
+      throw result.errors;
+    }
+  
+    // Create blog articles pages.
+    const categories = result.data.categories.edges;
+  
+    const CategoryTemplate = require.resolve('./src/templates/category.js');
+  
+    categories.forEach((category, index) => {
+      createPage({
+        path: `/category/${category.node.name}`,
+        component: CategoryTemplate,
+        context: {
+          name: category.node.name,
+        },
+      });
+    });
+  };
+  
---
+++ src/templates/category.js
@@ -0,0 +1,34 @@
+// category.js
+
+import React from 'react';
+import { graphql } from 'gatsby';
+
+export const query = graphql`
+  query Category($name: String!) {
+    category: strapiCategory(name: { eq: $name }) {
+      name
+      restaurants {
+        id
+        name
+      }
+    }
+  }
+`;
+
+const Category = ({ data }) => {
+  const restaurants = data.category.restaurants;
+  const category = data.category.name;
+
+  return (
+    <div>
+      <h1>{category}</h1>
+      <ul>
+        {restaurants.map(restaurant => {
+          return <li key={restaurant.id}>{restaurant.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Category;
